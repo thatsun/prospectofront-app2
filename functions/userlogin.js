e53a1e1f97d787590b420883877762ad52d2b4e2
ramon_ramirez <marluxia122@hotmail.com>
2020-09-07 20:06:59 -0600

    funciones

--- functions/userlogin.js
+++ functions/userlogin.js
@@ -4,27 +4,8 @@ const axios = require('axios')
 const API_ENDPOINT = 'https://serverprueba-con.herokuapp.com/user/login'
 
 exports.handler = async (event, context,callback) => {
-  let response
-  console.log(event.body);
-  try {
-    response = await fetch({
-        url:API_ENDPOINT,
-        body:event.body,
-        method: 'POST',
-        mode: 'cors',
-        body: event.body,
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-  } catch (err) {
-    return {
-      statusCode: err.statusCode || 500,
-      body: JSON.stringify({
-        error: err.message
-      })
-    }
-  }
+  console.log("soy el hadler");
+  
 
   return callback(null, {
         statusCode: 200,
